9d661a47defd95833a15df97de336fc6b4ea343e
raddix <rohitorrockstar@gmail.com>
2018-10-23 22:17:52 +0530

    FuzzySearch Added
    Added the fuzzysearch

--- app/src/main/java/com/censarone/areaguide/MainActivity.java
+++ app/src/main/java/com/censarone/areaguide/MainActivity.java
@@ -1,8 +1,16 @@
 package com.censarone.areaguide;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -13,6 +21,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableList;
 import com.tomtom.online.sdk.common.location.LatLng;
 import com.tomtom.online.sdk.map.BaseMarkerBalloon;
 import com.tomtom.online.sdk.map.Icon;
@@ -34,9 +43,15 @@ import com.tomtom.online.sdk.routing.data.RouteResult;
 import com.tomtom.online.sdk.routing.data.RouteType;
 import com.tomtom.online.sdk.search.OnlineSearchApi;
 import com.tomtom.online.sdk.search.SearchApi;
+import com.tomtom.online.sdk.search.api.SearchError;
+import com.tomtom.online.sdk.search.api.fuzzy.FuzzySearchResultListener;
 import com.tomtom.online.sdk.search.data.alongroute.AlongRouteSearchQueryBuilder;
 import com.tomtom.online.sdk.search.data.alongroute.AlongRouteSearchResponse;
 import com.tomtom.online.sdk.search.data.alongroute.AlongRouteSearchResult;
+import com.tomtom.online.sdk.search.data.fuzzy.FuzzySearchQuery;
+import com.tomtom.online.sdk.search.data.fuzzy.FuzzySearchQueryBuilder;
+import com.tomtom.online.sdk.search.data.fuzzy.FuzzySearchResponse;
+import com.tomtom.online.sdk.search.data.fuzzy.FuzzySearchResult;
 import com.tomtom.online.sdk.search.data.reversegeocoder.ReverseGeocoderSearchQueryBuilder;
 import com.tomtom.online.sdk.search.data.reversegeocoder.ReverseGeocoderSearchResponse;
 
@@ -47,7 +62,7 @@ import io.reactivex.observers.DisposableSingleObserver;
 import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback,
-        TomtomMapCallback.OnMapLongClickListener{
+        TomtomMapCallback.OnMapLongClickListener, LocationListener {
 
     private TomtomMap tomtomMap;
     private SearchApi searchApi;
@@ -59,8 +74,17 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     private Icon departureIcon;
     private Icon destinationIcon;
 
+    protected LocationManager locationManager;
+    protected LocationListener locationListener;
+
     private ImageButton btnSearch;
     private EditText editTextPois;
+
+    private LatLng a;
+    private LatLng b;
+
+    protected String latitude, longitude;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,7 +92,13 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
 
         btnSearch = findViewById(R.id.btn_main_poisearch);
         editTextPois = findViewById(R.id.edittext_main_poisearch);
@@ -79,6 +109,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         initTomTomServices();
         initUIViews();
         setupUIViewListeners();
+
+        justChecking();
     }
 
     @Override
@@ -100,7 +132,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     private void handleLongClick(@NonNull LatLng latLng) {
-        searchApi.reverseGeocoding(new ReverseGeocoderSearchQueryBuilder(latLng.getLatitude(), latLng.getLongitude()))
+        /*searchApi.reverseGeocoding(new ReverseGeocoderSearchQueryBuilder(latLng.getLatitude(), latLng.getLongitude()))
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new DisposableSingleObserver<ReverseGeocoderSearchResponse>() {
@@ -137,7 +169,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                         departurePosition = geocodedPosition;
                         createMarkerIfNotPresent(departurePosition, departureIcon);
                     }
-                });
+                });*/
     }
 
     private void createMarkerIfNotPresent(LatLng position, Icon icon) {
@@ -230,7 +262,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         return new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                handleSearchClick(v);
+                //handleSearchClick(v);
+                buttonCheck();
             }
 
             private void handleSearchClick(View v) {
@@ -264,7 +297,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 final Integer MAX_DETOUR_TIME = 1000;
                 final Integer QUERY_LIMIT = 10;
 
-                searchApi.alongRouteSearch(new AlongRouteSearchQueryBuilder(textToSearch, route.getCoordinates(), MAX_DETOUR_TIME).withLimit(QUERY_LIMIT))
+                /*searchApi.alongRouteSearch(new AlongRouteSearchQueryBuilder(textToSearch, route.getCoordinates(), MAX_DETOUR_TIME).withLimit(QUERY_LIMIT))
                         .subscribeOn(Schedulers.io())
                         .observeOn(AndroidSchedulers.mainThread())
                         .subscribe(new DisposableSingleObserver<AlongRouteSearchResponse>() {
@@ -302,7 +335,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                             public void onError(Throwable e) {
                                 handleApiError(e);
                             }
-                        });
+                        });*/
             }
         };
     }
@@ -334,4 +367,92 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     private void setupUIViewListeners() {}
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.i("LocationInfo","Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
+        a = new LatLng(location.getLatitude(),location.getLongitude());
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    public void justChecking()
+    {
+        FuzzySearchQuery fuzz = FuzzySearchQueryBuilder.create("Karkardooma").build();
+        searchApi.search(fuzz, new FuzzySearchResultListener() {
+            @Override
+            public void onSearchResult(FuzzySearchResponse fuzzySearchResponse) {
+                Log.i("Cordinates we get",fuzzySearchResponse.getResults().toString());
+            }
+
+            @Override
+            public void onSearchError(SearchError searchError) {
+                Log.e("TAG","Now what the fuck");
+            }
+        });
+
+        searchApi.search(fuzz)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(new DisposableSingleObserver<FuzzySearchResponse>() {
+                @Override
+                public void onSuccess(FuzzySearchResponse fuzzySearchResponse) {
+                    FuzzySearchResult result = fuzzySearchResponse.getResults().get(0);
+                    Toast.makeText(MainActivity.this,"The Lat-Lund position is this "+result.getPosition(),Toast.LENGTH_LONG).show();
+                    //String searchView.updateSearchResults(fuzzySearchResponse.getResults());
+
+                }
+
+                @Override
+                public void onError(Throwable throwable) {
+                    //searchView.showSearchFailedMessage(throwable.getMessage());
+                    //searchView.updateSearchResults(ImmutableList.<FuzzySearchResult>of());
+                }
+            });
+
+        Log.i("TAG","Just to wait here");
+    }
+
+    public void buttonCheck()
+    {
+        String text = editTextPois.getText().toString();
+        FuzzySearchQuery fuzz = FuzzySearchQueryBuilder.create(text).build();
+
+        searchApi.search(fuzz)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new DisposableSingleObserver<FuzzySearchResponse>() {
+                    @Override
+                    public void onSuccess(FuzzySearchResponse fuzzySearchResponse) {
+                        FuzzySearchResult result = fuzzySearchResponse.getResults().get(0);
+                        Toast.makeText(MainActivity.this,"The Lat-Lund position is this "+result.getPosition(),Toast.LENGTH_LONG).show();
+                        b = result.getPosition();
+                        drawRoute(a,b);
+                        //String searchView.updateSearchResults(fuzzySearchResponse.getResults());
+
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+                        //searchView.showSearchFailedMessage(throwable.getMessage());
+                        //searchView.updateSearchResults(ImmutableList.<FuzzySearchResult>of());
+                    }
+                });
+
+
+    }
 }
