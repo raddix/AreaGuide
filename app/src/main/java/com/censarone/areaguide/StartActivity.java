31d4183d62a9da3e4692e2cb85a89ffb0948b3ac
raddix <rohitorrockstar@gmail.com>
2018-10-27 18:51:44 +0530

    Added Categories

--- app/src/main/java/com/censarone/areaguide/StartActivity.java
+++ app/src/main/java/com/censarone/areaguide/StartActivity.java
@@ -1,83 +1,102 @@
 package com.censarone.areaguide;
 
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class StartActivity extends AppCompatActivity {
 
     private String[] catValues;
-    private boolean[] checkedCat;
 
-    private ArrayList<Integer> userSelectCat=new ArrayList<>();
+    private LinearLayout linearLayout;
+    private Button nextButton;
+
+    private View.OnClickListener checkBoxOnClick;
+
+    private Map<Integer,String> categorySelectedMap = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-        showDialogueBox();
+
+        initFields();
+        addCheckBoxesIntoLayout();
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(categorySelectedMap.size()<2)
+                    Toast.makeText(StartActivity.this, "Please select at Least two categories before continuing", Toast.LENGTH_SHORT).show();
+                else
+                {
+                    String[] selectedCategory = getSelectedCategory();
+                    System.out.println("The size of the selected cateogory is "+selectedCategory.length);
+                    Intent intent = new Intent(StartActivity.this,MainActivity.class);
+                    intent.putExtra("selectedCategory",selectedCategory);
+                    startActivity(intent);
+                }
+            }
+        });
+
+
     }
 
-    private void showDialogueBox() {
-        catValues=getResources().getStringArray(R.array.Categories);
-        checkedCat=new boolean[catValues.length];
-        AlertDialog.Builder mBuilder=new AlertDialog.Builder(StartActivity.this);
-        mBuilder.setTitle("Choose Category");
-        mBuilder.setMultiChoiceItems(catValues, checkedCat, new DialogInterface.OnMultiChoiceClickListener() {
+    private String[] getSelectedCategory() {
+        String[] selectedCategory = new String[categorySelectedMap.size()];
+        int i=0;
+
+        for(Map.Entry<Integer,String> entry : categorySelectedMap.entrySet())
+        {
+            selectedCategory[i] = entry.getValue();
+            i++;
+        }
+
+        return selectedCategory;
+    }
+
+    private void addCheckBoxesIntoLayout() {
+        for(int i = 0; i < catValues.length; i++) {
+            CheckBox cb = new CheckBox(getApplicationContext());
+            cb.setOnClickListener(checkBoxOnClick);
+            cb.setId(i);
+            cb.setText(catValues[i].toUpperCase());
+            linearLayout.addView(cb);
+        }
+    }
+
+    private void initFields() {
+        linearLayout = findViewById(R.id.linear_layout);
+        catValues = getResources().getStringArray(R.array.Categories);
+        nextButton = findViewById(R.id.next_button);
+
+        checkBoxOnClick = new View.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
-                if (isChecked) {
-                    if (!userSelectCat.contains(which)) {
-                        userSelectCat.add(which);
+            public void onClick(View v) {
+                boolean isChecked = ((CheckBox) v).isChecked();
 
-                    } else {
-                        userSelectCat.remove(which);
-                    }
-                }
-            }
-        }).setCancelable(false).setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-
-                String item = "";
-                for (int i = 0; i < userSelectCat.size(); i++) {
-                    if (i != userSelectCat.size() - 1)
-                        item = item + catValues[userSelectCat.get(i)] + ",";
-                    else
-                        item = item + catValues[userSelectCat.get(i)];
-
-                }
-
-                Intent intent = new Intent(StartActivity.this,MainActivity.class);
-                intent.putExtra("category",userSelectCat);
-                startActivity(intent);
-
-
-            }
-        }).setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-            }
-        });
-        mBuilder.setNeutralButton("Clear All", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                userSelectCat.clear();
-                for(int i=0;i<checkedCat.length;i++)
+                if(isChecked)
                 {
-                    checkedCat[i]=false;
-
+                    if(categorySelectedMap.size()>=5)
+                    {
+                        Toast.makeText(StartActivity.this,"Sorry, but we only have time for five destinations !!",Toast.LENGTH_SHORT).show();
+                        ((CheckBox) v).setChecked(false);
+                    }
+                    else
+                        categorySelectedMap.put(v.getId(),catValues[v.getId()]);
                 }
+                else
+                    categorySelectedMap.remove(v.getId());
             }
-        });
-        AlertDialog mDialog=mBuilder.create();
-        mDialog.show();
+        };
     }
 }
